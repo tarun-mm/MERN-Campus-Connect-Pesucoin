d1087f42610fa33fb50562444ab65d3152852fd3
Tarun m <tarunsetty2002@gmail.com>
2021-12-08 09:48:06 +0530

    Spaces cleanup"

--- client/src/Pages/Spaces/spaces.js
+++ client/src/Pages/Spaces/spaces.js
@@ -12,25 +12,12 @@ class Spaces extends React.Component {
   }
 
   componentDidMount = () =>{
-    var elems = []
-
     axios({
       url: "http://localhost:8081/spaces.json",
       method: "GET",
       headers: { "Content-Type": "application/json" },
     })
       .then((res) => {
-        for(var iterable = 0; iterable < res.data.length; iterable++){
-          var temp = []
-          temp.push(res.data[iterable]["_id"])
-          temp.push(res.data[iterable]["space_name"])
-          temp.push(res.data[iterable]["venue"])
-          temp.push(res.data[iterable]["date"])
-          temp.push(res.data[iterable]["time"])
-          elems.push(temp)
-        }
-        // console.log(elems)
-
         this.setState({
           space_elems: res.data,
         })
@@ -46,12 +33,9 @@ class Spaces extends React.Component {
         <section id="section" className="section">
           <div className="box-main" style={{display: "flex", flexWrap: "wrap"}}>
             {
-              // console.log(this.state.space_elems),
               this.state.space_elems &&
               this.state.space_elems.map((item, index) => {
                 var marLeft = "1%"
-                // if(index%3) marLeft = "2%"
-                // console.log(item)
                 console.log(index)
                 return ( <>
                   <div className="card" key={item["_id"]} style={{width: "27%", marginLeft: marLeft, marginBottom: "1%"}}>
